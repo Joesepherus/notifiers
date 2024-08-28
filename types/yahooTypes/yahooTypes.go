fb96a16cf7c0967c5f11b746313aae91a16788ff
Joseph Maloch <jozefmaloch@gmail.com>
2024-08-28 16:22:31 +0800

    Add yahooTypes.

---
+++ types/yahooTypes/yahooTypes.go
@@ -0,0 +1,13 @@
+package yahooTypes
+
+type StockResponse struct {
+	Chart struct {
+		Result []struct {
+			Meta struct {
+				Currency           string  `json:"currency"`
+				Symbol             string  `json:"symbol"`
+				RegularMarketPrice float64 `json:"regularMarketPrice"`
+			} `json:"meta"`
+		} `json:"result"`
+	} `json:"chart"`
+}
