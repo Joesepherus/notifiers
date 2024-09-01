a573548c2651d2a5b3c657a6a9f8b1b4ce06b5b8
Joseph Maloch <jozefmaloch@gmail.com>
2024-09-01 13:33:32 +0800

    Add the modal to base instead of index so that it can be accessible even when clicking on the header login and sign up links.

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,38 @@
 function showModal() {
-    const modal = document.getElementById('signup-modal');
-    modal.classList.add('show');
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          closeSignUpForm();
-        }
-      };
-  }
+  const modal = document.getElementById("signup-modal");
+  modal.classList.add("show");
+  window.onclick = function (event) {
+    if (event.target.classList.contains("modal")) {
+      closeSignUpForm();
+    }
+  };
+}
 
-  function closeSignUpForm() {
-    const modal = document.getElementById('signup-modal');
-    modal.classList.remove('show');
-  }
+function closeSignUpForm() {
+  const modal = document.getElementById("signup-modal");
+  modal.classList.remove("show");
+}
 
-  function showSignUpForm() {
-    const signupForm = document.getElementById('signup-form');
-    const loginForm = document.getElementById('login-form');
-    signupForm.classList.add('active-form');
-    loginForm.classList.remove('active-form');
-  }
+function showSignUpForm() {
+  const signupForm = document.getElementById("signup-form");
+  const loginForm = document.getElementById("login-form");
+  signupForm.classList.add("active-form");
+  loginForm.classList.remove("active-form");
+}
 
-  function showLoginForm() {
-    const signupForm = document.getElementById('signup-form');
-    const loginForm = document.getElementById('login-form');
-    loginForm.classList.add('active-form');
-    signupForm.classList.remove('active-form');
-  }
+function showLoginForm() {
+  const signupForm = document.getElementById("signup-form");
+  const loginForm = document.getElementById("login-form");
+  loginForm.classList.add("active-form");
+  signupForm.classList.remove("active-form");
+}
+
+function openModalShowLoginForm() {
+  showModal();
+  showLoginForm();
+}
+
+function openModalShowSignUpForm() {
+  showModal();
+  showSignUpForm();
+}
