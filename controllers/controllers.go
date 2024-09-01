ed9b9dbba010fcba5be829823f4b28ba839dabac
Joseph Maloch <jozefmaloch@gmail.com>
2024-09-01 15:40:15 +0800

    Remove login and sign up page and remove GetAlertsPage from controllers.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -12,7 +12,6 @@ import (
 	"notifiers/templates"
 	"os"
 	"strconv"
-	"text/template"
 )
 
 func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
@@ -78,24 +77,6 @@ func RestApi() {
 	// Define the route for getting untriggered alerts
 	http.Handle("/api/alerts", authMiddleware.TokenAuthMiddleware(http.HandlerFunc(alertsController.GetAlerts)))
 
-	// Define the route for serving the signup page
-	http.HandleFunc("/sign-up", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			http.ServeFile(w, r, "templates/signup.html")
-		} else {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		}
-	})
-
-	// Define the route for serving the login page
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			http.ServeFile(w, r, "templates/login.html")
-		} else {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		}
-	})
-
 	// Authentication routes
 	http.HandleFunc("/api/sign-up", authController.SignUp)
 	http.HandleFunc("/api/login", authController.Login)
@@ -112,24 +93,4 @@ func RestApi() {
 
 	log.Printf("Starting server on :%d...\n", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
-}
-
-func GetAlertsPage(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-	alerts, err := alertsService.GetAlerts()
-	if err != nil {
-		http.Error(w, "Failed to fetch alerts", http.StatusInternalServerError)
-	}
-
-	tmpl, err := template.ParseFiles("templates/alerts.html")
-	if err != nil {
-		http.Error(w, "Failed to load template", http.StatusInternalServerError)
-		return
-	}
-
-	if err := tmpl.Execute(w, alerts); err != nil {
-		http.Error(w, "Failed to render template", http.StatusInternalServerError)
-	}
 }
