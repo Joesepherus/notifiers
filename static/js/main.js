075d7717795744e807dd338191e2955d36c9357f
Joseph Maloch <jozefmaloch@gmail.com>
2024-09-01 13:06:50 +0800

    Add login to the auth form.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-function showSignUpForm() {
+function showModal() {
     const modal = document.getElementById('signup-modal');
     modal.classList.add('show');
     window.onclick = function(event) {
@@ -11,4 +11,18 @@ function showSignUpForm() {
   function closeSignUpForm() {
     const modal = document.getElementById('signup-modal');
     modal.classList.remove('show');
+  }
+
+  function showSignUpForm() {
+    const signupForm = document.getElementById('signup-form');
+    const loginForm = document.getElementById('login-form');
+    signupForm.classList.add('active-form');
+    loginForm.classList.remove('active-form');
+  }
+
+  function showLoginForm() {
+    const signupForm = document.getElementById('signup-form');
+    const loginForm = document.getElementById('login-form');
+    loginForm.classList.add('active-form');
+    signupForm.classList.remove('active-form');
   }
